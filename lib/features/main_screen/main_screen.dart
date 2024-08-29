8866ff4b01c4aa72634320da7d62670085cf185e
Valeriia Radzivilo <v.radzivilo.ip14@kpi.ua>
2024-08-29 23:38:36 +0300

    - Connected to Java Spring

--- lib/features/main_screen/main_screen.dart
+++ lib/features/main_screen/main_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:zip_way/features/color_detection/color_detection.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(final BuildContext context) {
-    return const Placeholder();
+    return const ColorDetection();
   }
 }
